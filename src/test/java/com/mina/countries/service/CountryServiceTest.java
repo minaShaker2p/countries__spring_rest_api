32f01dbc5b1797b3596144f6a073f67273a4e7de
MinaRezkalla <mina.shaker@appsfactory.de>
2020-06-19 17:10:38 +0200

    remove conflict dependency

---
+++ src/test/java/com/mina/countries/service/CountryServiceTest.java
@@ -0,0 +1,15 @@
+package com.mina.countries.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayName("Hello Mina test class")
+class CountryServiceTest {
+
+    @Test
+    void getAllCountries() {
+        assertTrue(true);
+    }
+}
