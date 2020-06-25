d2b1140c3eb140407ea4c6753fc7cead4b21cf38
MinaRezkalla <mina.shaker@appsfactory.de>
2020-06-25 15:59:18 +0200

    start testing countries repository

---
+++ src/test/java/com/mina/countries/repository/CountryRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.mina.countries.repository;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+class CountryRepositoryTest {
+
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private EntityManager entityManager;
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @Test
+    void injectionComponentsAreNotNull() {
+        assertThat(dataSource).isNotNull();
+        assertThat(jdbcTemplate).isNotNull();
+        assertThat(entityManager).isNotNull();
+        assertThat(countryRepository).isNotNull();
+    }
+
+}
